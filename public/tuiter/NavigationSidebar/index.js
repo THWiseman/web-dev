54f23c707a3f0e0e03f6003c23d6c5efa8dfccdb
THWiseman <77803506+twiseman1992@users.noreply.github.com>
2022-02-28 20:43:07 -0500

    Finished a5

---
+++ public/tuiter/NavigationSidebar/index.js
@@ -0,0 +1,31 @@
+const NavigationSidebar = () => {
+    return(`
+            <div class="list-group">
+                <a class="list-group-item" href="/">
+                    <i class="fab fa-twitter"></i></a>
+                <a href="../home.html" class="list-group-item list-group-item-action">
+                    <i class="fa-solid fa-house-chimney"></i><span class="d-none d-xl-inline-block"> Home</span> </a>
+                <a href="../explore.html" class="list-group-item list-group-item-action active">
+                    <i class="fa-solid fa-hashtag"></i>   <span class="d-none d-xl-inline-block"> Explore</span></a>
+                <a href="../notifications.html" class="list-group-item list-group-item-action">
+                    <i class="fa-solid fa-bell"></i>   <span class="d-none d-xl-inline-block"> Notifications</span></a>
+                <a href="../messages.html" class="list-group-item list-group-item-action" tabindex="-1" aria-disabled="true">
+                    <i class="fa-solid fa-envelope"></i>    <span class="d-none d-xl-inline-block"> Messages</span></a>
+                <a href="../bookmarks.html" class="list-group-item list-group-item-action" tabindex="-1" aria-disabled="true">
+                    <i class="fa-solid fa-bookmark"></i>    <span class="d-none d-xl-inline-block"> Bookmarks</span></a>
+                <a href="../lists.html" class="list-group-item list-group-item-action" tabindex="-1" aria-disabled="true">
+                    <i class="fa-solid fa-list"></i>   <span class="d-none d-xl-inline-block"> Lists</span></a>
+                <a href="../profile.html" class="list-group-item list-group-item-action" tabindex="-1" aria-disabled="true">
+                    <i class="fa-solid fa-user"></i>    <span class="d-none d-xl-inline-block"> Profile</span></a>
+                <a href="../navigation.html" class="list-group-item list-group-item-action" tabindex="-1" aria-disabled="true">
+                    <i class="fa-solid fa-ellipsis"></i>
+                    <span class="d-none d-xl-inline-block"> More</span></a>
+            </div>
+            <div class="d-grid mt-2">
+                <a href="../tweet.html"
+                   class="btn btn-primary btn-block rounded-pill">
+                    Tweet</a>
+            </div>
+    `);
+}
+export default NavigationSidebar;
