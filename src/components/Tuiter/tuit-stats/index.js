67999be1779ef14b23361a68666bf2f4e47bf1fc
THWiseman <77803506+twiseman1992@users.noreply.github.com>
2022-04-06 15:34:08 -0400

    finished a8

--- src/components/Tuiter/tuit-stats/index.js
+++ src/components/Tuiter/tuit-stats/index.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import {useDispatch} from "react-redux";
+import {updateTuit} from "../actions/tuits-actions"
 
-const TuitStats = ({tuit}) => {
+const TuitStats = (tuit) => {
     const dispatch = useDispatch();
-    const likeTuit = () => {
-        dispatch({type: 'like-tuit', tuit});
-    };
+    console.log(tuit);
     return (
-    <span onClick={likeTuit}>
-    {
-        tuit.liked &&
-        <i className="fas fa-heart me-1"
-           style={{color: 'red'}}></i>
-    }
-        {
-            !tuit.liked &&
-            <i className="far fa-heart me-1"></i>
-        }
-        {tuit.stats && tuit.stats.likes}
-    </span>
-);
+           <div>
+     Likes: {tuit.likes.toString()}
+               <i onClick={() => updateTuit(dispatch, {
+                   ...tuit,
+               likes : tuit.likes + 1})
+               } className="far fa-thumbs-up ms-2"></i>
+               Dislikes: {tuit.dislikes.toString()}
+               <i onClick={() => updateTuit(dispatch, {
+                   ...tuit,
+                   dislikes : tuit.dislikes + 1})
+               } className="far fa-thumbs-down ms-2"></i>
+        </div>);
 }
 export default TuitStats;
