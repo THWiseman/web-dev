40de2ecec56b14153ef395363dc6d8ee9430b03a
THWiseman <77803506+twiseman1992@users.noreply.github.com>
2022-04-06 15:53:14 -0400

    a8 done

--- src/components/Tuiter/tuit-stats/index.js
+++ src/components/Tuiter/tuit-stats/index.js
@@ -4,15 +4,14 @@ import {updateTuit} from "../actions/tuits-actions"
 
 const TuitStats = (tuit) => {
     const dispatch = useDispatch();
-    console.log(tuit);
     return (
            <div>
-     Likes: {tuit.likes.toString()}
+     Likes: {tuit.likes}
                <i onClick={() => updateTuit(dispatch, {
                    ...tuit,
                likes : tuit.likes + 1})
                } className="far fa-thumbs-up ms-2"></i>
-               Dislikes: {tuit.dislikes.toString()}
+               Dislikes: {tuit.dislikes}
                <i onClick={() => updateTuit(dispatch, {
                    ...tuit,
                    dislikes : tuit.dislikes + 1})
